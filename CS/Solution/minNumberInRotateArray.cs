8172ae9aa55fd3ffb9cee69ede1588a47e107d63
gjj2828 <gjj2828@gmail.com>
2020-04-10 23:17:24 +0800

    添加minNumberInRotateArray算法的测试用例

--- CS/Solution/minNumberInRotateArray.cs
+++ CS/Solution/minNumberInRotateArray.cs
@@ -35,8 +35,9 @@ namespace minNumberInRotateArray
             int[][] q = { new int[] { 3, 4, 5, 1, 2 }
                 , new int[] { 2, 4, 5, 1, 2 }
                 , new int[] { 2, 4, 5, 1, 2, 2, 2, 2, 2 }
-                , new int[] { 2, 2, 2, 2, 2, 1, 2, 2, 2 }};
-            int[] a = { 1, 1, 1, 1 };
+                , new int[] { 2, 2, 2, 2, 2, 1, 2, 2, 2 }
+                , new int[] { 1, 1, 1 } };
+            int[] a = { 1, 1, 1, 1, 1 };
             int num = Math.Min(q.Length, a.Length);
             for(int i = 0; i < num; i++)
             {
